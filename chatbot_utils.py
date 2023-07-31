faa5e8cd3ad39f50bf857a36da7d4188b3d3ef4a
INSAEN <pratham1986@gmail.com>
2023-07-31 13:25:52 +0530

    App created

---
+++ chatbot_utils.py
@@ -0,0 +1,78 @@
+import os
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+import requests
+from wit import Wit
+
+os.environ["OPENAI_API_KEY"] = "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"
+client = Wit("SPOYTVVVGBOKG4NLGM73X6BVJRF26HU4")
+
+def getLangs():
+    url = "https://text-translator2.p.rapidapi.com/getLanguages"
+    headers = {
+        "X-RapidAPI-Key": "2820853ee3mshf9b0447869d2d4cp1c6e27jsned29a81f764a",
+        "X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers)
+    langs=response.json()["data"]["languages"]
+    return langs
+
+doc_reader = PdfReader('../ga.pdf')
+raw_text = ''
+for i, page in enumerate(doc_reader.pages):
+    text = page.extract_text()
+    if text:
+        raw_text += text
+
+text_splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200, #striding over the text
+    length_function = len,
+)
+texts = text_splitter.split_text(raw_text)
+
+embeddings = OpenAIEmbeddings()
+docsearch = FAISS.from_texts(texts, embeddings)
+chain = load_qa_chain(OpenAI(),
+                      chain_type="stuff")
+
+def translate(text,src,code):
+    url = "https://text-translator2.p.rapidapi.com/translate"
+    payload = {
+        "source_language": src,
+        "target_language": code,
+        "text": text
+    }
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": "2820853ee3mshf9b0447869d2d4cp1c6e27jsned29a81f764a",
+        "X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
+    }
+    response = requests.post(url, data=payload, headers=headers)
+    return response.json()["data"]["translatedText"]
+
+def reply(name,prompt,code):
+    t_query=prompt
+    if(code!='en'):
+        t_query=translate(prompt,code,"en")
+    chain.llm_chain.prompt.template="Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Answer like customer support in detail. Use this customer information to make the conversation more human like. "+name+". Use bullet points where required.\n\n{context}\n\nQuestion: {question}\nHelpful Answer:"
+    docs = docsearch.similarity_search(t_query)
+    text=chain.run(input_documents=docs, question=t_query)
+    witresponse=client.message(t_query)
+    if(len(witresponse["intents"])==0):
+        text=text
+    elif(witresponse["intents"][0]["name"])=="orders":
+        text=text+"\n You can view your orders at https://www.abc.com/gp/your-account/order-history"
+    elif(witresponse["intents"][0]["name"])=="returns":
+        text=text+"\n You can view your returns at https://www.abc.com/gp/css/returns/homepage.html"
+    elif(witresponse["intents"][0]["name"])=="account_management":
+        text=text+"\n You can view/edit your account details at https://www.abc.com/gp/css/accounts.html"
+    if(code=="en"):
+        return text
+    else:
+        return translate(text,"en",code)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,44 @@
 import streamlit as st
+import chatbot_utils
+st.title('ABC Chatbot')
 
-st.title('🎈 App Name')
+langs=chatbot_utils.getLangs()
+names = []
+code="en"
+for language in langs:
+    names.append(language['name'])
+with st.sidebar:
+    st.write('Please enter your name')
+    name = st.text_input('Name')
 
-st.write('Hello world!')
+    langName = st.selectbox(
+        "Select Language",
+        names,
+        index=21,
+        label_visibility="visible",
+    )
+    for language in langs:
+        if language['name'] == langName:
+            code = language['code']
+            break
+if name:
+    if ("messages" not in st.session_state.keys()):
+        st.session_state.messages = [{"role": "ABC", "content": f"Hello! {name}, Welcome to ABC Customer service. How can I help you?"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input(disabled=not (name)):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    if st.session_state.messages[-1]["role"] != "ABC":
+        with st.chat_message("ABC"):
+            with st.spinner():
+                response = chatbot_utils.reply(name,prompt,code)
+                # response = "This is a test response"
+                st.write(response) 
+        message = {"role": "ABC", "content": response}
+        st.session_state.messages.append(message)
